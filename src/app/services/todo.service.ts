a6aa879cbdf9bd6592f31dab7c5124e757e6e363
cross <aiyinsitan@outlook.com>
2018-01-17 21:35:39 +0800

    fixed: have to return a Obervable clearly and directly. While not this.http.get..., why?

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
+import { Observable } from 'rxjs/Rx';
+
 @Injectable()
 export class TodoService {
 
@@ -8,9 +10,10 @@ export class TodoService {
 
 
   getMessage() {
-    return this.http
-        .get(`https://api.github.com/orgs/angular/members?page=1&per_page=5`)
-        .map(res => res.json())
+    return Observable.of("this is a string message.")
+    // return this.http
+    //     .get(`https://api.github.com/orgs/angular/members?page=1&per_page=5`)
+    //     .map(res => res.json())
   }
 
 
