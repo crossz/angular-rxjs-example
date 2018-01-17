f635b2e161e23e36bef874212d6305a945f22299
cross <aiyinsitan@outlook.com>
2018-01-17 20:42:12 +0800

    first commit: rxjs + router.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Observable } from 'rxjs/Rx';
+// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Rxjs Demo';
+
+
+  
+
+  constructor(
+    // @Inject('todoService') private service,
+    private route: ActivatedRoute,
+    private router: Router) {
+      this.title = 'hello world.';
+
+
+      console.log(this.route.params)
+      // get param method 1
+      console.log(this.route.snapshot.paramMap.get('filter')); 
+      // get param method 2
+      this.route.paramMap.subscribe(  
+        params => {
+          this.title = params.get('filter');
+          console.log(this.title);
+        }
+      );
+
+    
+  
+
+
+      // Observable.from(this.route.params)
+      this.route.params
+      ._do( val => console.log(val))
+        .pluck('filter')
+        .subscribe(filter => {
+          console.log(filter);
+        })
+  
+
+
+    }
+
+  ngOnInit() {
+
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
